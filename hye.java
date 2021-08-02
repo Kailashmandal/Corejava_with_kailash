b31b40f3fe39443e8944927ce2621891f46d02fa
kailash <hashtagkailash@gmail.com>
2021-08-02 12:10:23 +0530

    changing variable

--- hye.java
+++ hye.java
@@ -4,6 +4,7 @@ class hyejava{
     public static void main(String[] args){
         int x=10;
         int y=9;
-        System.out.print(x+y);
+        int z=11;
+        System.out.print(x+y+z);
     }
 }

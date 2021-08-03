7d8c4fca313b8c060c7c0c287d1335b0375934ea
kailash <hashtagkailash@gmail.com>
2021-08-03 11:53:03 +0530

    print Hye java

--- hye.java
+++ hye.java
@@ -2,9 +2,7 @@
 
 class hyejava{
     public static void main(String[] args){
-        int x=10;
-        int y=9;
-        int z=11;
-        System.out.print(x+y+z);
+        
+        System.out.print("Hye java");
     }
 }

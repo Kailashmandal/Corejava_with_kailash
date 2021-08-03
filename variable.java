0863bf969f4e538983c483773aa6fefc8934c779
kailash <hashtagkailash@gmail.com>
2021-08-03 12:22:01 +0530

    Varibles and primitive Data types

---
+++ variable.java
@@ -0,0 +1,28 @@
+public class variable {
+    public static void main(String[] args){
+     /** variable is like a container to store the data of any type. or simply is a memory location*/ 
+     // syntax of declaration   ->   data_type variable_name ;
+     //primitive data type 
+
+     boolean flag=true; // used for tracking true/flase conditions
+
+     byte i=0;  // integer value range from -128 to 127(1 btye)
+     
+     char c='A'; // for single charactor(2 btyes)
+     
+     short s=5000;// integer value (2 bytes)
+     
+     int my_int =50;// integer value (4 btyes)
+     
+     float my_float=50.5f;// floating numbers(4 btyes)
+     
+     long my_long=40000000;// highest integer range(8 bytes)
+     
+     double my_double=40000000.5d; // highest floating number range(8 bytes)
+
+     System.out.println(flag);
+     System.out.println(i);
+     System.out.println(my_double);
+     /**we can print any value bye this statement */
+    }
+}

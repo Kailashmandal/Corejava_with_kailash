05876dd644a3bf3d3576f903fdce95d4f2e02632
Kailashmandal <88301928+Kailashmandal@users.noreply.github.com>
2021-09-18 11:20:32 +0530

    2D arrays in java basics

--- Array2.java
+++ Array2.java
@@ -1,16 +1,54 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Array2 {
+
+    /* 2D arrays are array of arrays also called matrix 
+
+    syntax of declaration :
+
+    data_type [][] ref_name=new data_type[row][column];
+
+
+     * while initalising the 2cd array it is needed to give row size 
+       but not column size .
+    */
     public static void main(String[] args) {
-        /* cloning a array in java using clone function */
+        Scanner in=new Scanner(System.in);
 
-        int arr[]={ 1,2 ,3 ,4 ,5 ,6};
-    
-        int crr[]= arr.clone(); // clone function creates a deep copy 
+        int [][] arr=new int[3][4];
 
-        // printing the crr 
-        System.out.println("Crr is : ");
-        for (int i : crr) {
-            System.out.print(i+" ");
+        // taking input in matrix
+        System.out.println("enter the elements ");
+        for(int i=0;i<arr.length;i++){ // for each row 
+
+            for(int j=0;j<arr[i].length;j++){ // each column of each row
+              arr[i][j]=in.nextInt();
+            }
         }
+
+        // printing the matrix 
+        System.out.println("the matrix is : ");
+        for(int i=0;i<arr.length;i++){ // for each row 
+
+            for(int j=0;j<arr[i].length;j++){ // each column of each row
+              System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        // OR
+         System.out.println("By toString method : ");
+        for(int i=0;i<arr.length;i++){
+            System.out.println(Arrays.toString(arr[i]));
+        }
+
+        //OR by using for-each loop
+        System.out.println();
+        for (int[] is : arr) {
+            System.out.println(Arrays.toString(is));
+        }
+        in.close();
 
     }
 }
